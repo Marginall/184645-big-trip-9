bc71443235299c2fd55b722271f771e90f270b89
marginall <mistmee2016@gmail.com>
2019-08-13 16:20:47 +0300

    new components

--- src/main.js
+++ src/main.js
@@ -1,3 +1,27 @@
-'use strict';
-console.log(1);
+import {createTripInfo} from './components/trip';
+import {createMenu} from './components/menu';
+import {createFilter} from './components/filter';
+import {createSort} from './components/sort';
+import {createCard} from './components/card';
+import {createEventForm} from './components/event-edit';
 
+const renderComponents = (container, component, place) => {
+  container.insertAdjacentHTML(place, component);
+};
+
+const tripInfoContainer = document.querySelector(`.trip-info`);
+const tripControls = document.querySelector(`.trip-controls`);
+const tripEventsContainer = document.querySelector(`.trip-events`);
+const tripDaysContainer = `<ul class="trip-days"></ul>`;
+
+renderComponents(tripInfoContainer, createTripInfo(), `afterbegin`);
+renderComponents(tripControls, createMenu(), `afterbegin`);
+renderComponents(tripControls, createFilter(), `beforeend`);
+renderComponents(tripEventsContainer, createSort(), `beforeend`);
+renderComponents(tripEventsContainer, tripDaysContainer, `beforeend`);
+
+const daysList = tripEventsContainer.querySelector(`.trip-days`);
+renderComponents(daysList, createCard(), `afterbegin`);
+
+const dayEventsList = daysList.querySelector(`.trip-events__list`);
+renderComponents(dayEventsList, createEventForm(), `afterbegin`);
